4f8886e6a86cff56d5833c30d2a7862c9bd3c5fe
MrKeesLer <keeslergame@gmail.com>
2019-08-13 12:47:45 +0200

    Add the project

---
+++ android/src/fr/fireowls/fireband/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package fr.fireowls.fireband;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import fr.fireowls.fireband.FireBand;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FireBand(), config);
+	}
+}
