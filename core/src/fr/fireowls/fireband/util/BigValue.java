244e952d9d2602934d6a1141a0489a4dea757486
GeoJo <vaniscotte.geoffrey@hotmail.com>
2019-08-15 13:46:21 +0200

    Ajout final de la class BigValue

--- core/src/fr/fireowls/fireband/util/BigValue.java
+++ core/src/fr/fireowls/fireband/util/BigValue.java
@@ -1,21 +1,18 @@
 package fr.fireowls.fireband.util;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 
 /**
- * Classe permettant de gerer de très grand nombres ayant pour seul limite la taille d'une arraylist
+ * Classe permettant de gerer de très grand nombres ayant pour seul limite la taille d'un BigInteger
  * dans chaque index est stocké un nombre jusqu'a trois chiffres.
- * Cette classe a initialement été concu pour le jeu de type mobile de type clicker 'FireBand'
+ * Cette classe a initialement été concu pour le jeu de type mobile de type clicker 'FireBand' 
  * developé par FireOwls.
  * @author vaniscotte-geoffrey
  */
 public class BigValue {
 
-    /**
-     * ArrayList d'Integer qui stocke les entier par trois afin de differencier les unités des
-     * milliers d'unité, millions , ...etc
-     */
-    private ArrayList<Integer> valueList;
+    private BigInteger value;
     /**
      * Character qui sera attribué à l'unité que l'on traite, il est surtout utile lors de l'affichage
      * (ex : $, €, ...ect)
@@ -24,7 +21,7 @@ public class BigValue {
     /**
      * Tableau de String permettant l'affichage des valeurs
      */
-    private String c[] = {"", "k", "M", "B", "T"};
+    private String divition[] =     {"", "k", "M", "B", "T"};
 
     /**
      *
@@ -32,58 +29,60 @@ public class BigValue {
      */
     public BigValue(char unite){
         this.unite = unite;
-        valueList = new ArrayList<>();
-        valueList.add(new Integer(0));
+        value = new BigInteger("0");
     }
-
-    /**
-     * Additionne une nombre au BigValue
-     * @param number le nombre que vous voulez ajouter
-     */
-    public void add(long number){
-        ArrayList<Integer> list = numberToArrayList(number);
-        for(int i = list.size()-1; i >= 0; i--){
-            try{
-                valueList.get(i);
-            }catch (IndexOutOfBoundsException e) {
-                valueList.add(new Integer(0));
-            }
-            if(valueList.get(i) + list.get(i) >= 1000) {
-                valueList.set(i, (valueList.get(i) + list.get(i))-1000);
-                if(i == 0) valueList.add(1);
-            }else {
-                valueList.set(i, valueList.get(i) + list.get(i));
-            }
-        }
+    
+    public BigInteger getValue() {
+    	return value;
     }
-
-    /**
-     * Retourne un ArrayList correspondant au nombre passer en paramètre
-     * sous la forme : index 0 : unité, index 1 : millier, etc...
-     * @param number le nombre à convertir
-     * @return une Arraylist d'Integer
-     */
-    public static ArrayList<Integer> numberToArrayList(long number){
-        ArrayList<Integer> list = new ArrayList<>();
-        long initialNumber = number;
-        while(number/1000 != 0){
-            list.add((int)(initialNumber - number/1000*1000)); //cette formule permet de recuper les trois derniers chiffres d'un nombre
-            number = number / 1000;
-        }
-        list.add(new Integer((int)number));
-        return list;
+    
+    public void subtract(BigValue number) {
+    	value = value.subtract(number.getValue());
     }
+    
+    public void subtract(BigInteger number) {
+    	value = value.subtract(number);
+    }
+    
+    public void subtract(int number) {
+    	subtract(new BigInteger(number+""));
+    }
+    
+    public void subtract(long number) {
+    	subtract(new BigInteger(number+""));
+    }
+    
+    public void add(BigValue number) {
+    	value = value.add(number.getValue());
+    }
+    
+    public void add(BigInteger number) {
+    	value = value.add(number);
+    }
+    
+    public void add(int number) {
+    	add(new BigInteger(number+""));
+    }
+    
+    public void add(long number) {
+    	add(new BigInteger(number+""));
 
+    }
+    
+    private String getSubDivition() {
+    	int numberOfChars = value.toString().length();
+    	while(numberOfChars % 3 != 0) numberOfChars++;
+    	return divition[numberOfChars/3-1];
+    }
+    
     /**
      * Affiche le bigvalue (ex : 789 $, 456 k$, 123 M$) si le caractere d'unité est '$'
      */
     public String toString() {
-        String unit = "";
-        if(c[valueList.size()-1] == null) {
-            unit = "";
-        } else {
-            unit = c[valueList.size()-1];
-        }
-        return valueList.get(valueList.size()-1) + " " + unit + unite;
+    	if(value.toString().length() <= 3) return value.toString() + unite;
+    	if(value.toString().length() % 3 == 0) return value.toString().substring(0, 3) + getSubDivition() + unite;
+    	if(value.toString().length() % 3 == 1) return value.toString().substring(0, 1) + getSubDivition() + unite;
+    	if(value.toString().length() % 3 == 2) return value.toString().substring(0, 2) + getSubDivition() + unite;
+    	return "Error";
     }
 }
