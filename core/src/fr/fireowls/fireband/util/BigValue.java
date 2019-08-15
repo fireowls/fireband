612f2f467326a5d22adc898983d3dfbfe01e831f
vaniscotte-geoffrey <vaniscotte.geoffrey@hotmail.com>
2019-08-15 17:56:23 +0200

    Update de BigValue et création de la class Constant

--- core/src/fr/fireowls/fireband/util/BigValue.java
+++ core/src/fr/fireowls/fireband/util/BigValue.java
@@ -17,25 +17,66 @@ public class BigValue {
      * Character qui sera attribué à l'unité que l'on traite, il est surtout utile lors de l'affichage
      * (ex : $, €, ...ect)
      */
-    private char unite;
+    private String unite;
     /**
      * Tableau de String permettant l'affichage des valeurs
      */
-    private String divition[] =     {"", "k", "M", "B", "T"};
+    private String divition[] = Constant.divition;
+
+    /**
+     * Nombre maximum stockable dans le BitInteger
+     */
+    private BigInteger maxValue = new BigInteger("100000000000000");
 
     /**
      *
      * @param unite correspond au caractère afficher lors de l'affichage du nombre
      */
-    public BigValue(char unite){
+    public BigValue(String unite){
         this.unite = unite;
         value = new BigInteger("0");
     }
-    
-    public BigInteger getValue() {
-    	return value;
+
+    /**
+     *
+     * @return la chaine de charactère affiché
+     */
+    public String getUnite(){
+        return this.unite;
     }
-    
+
+    /**
+     * Set la chaine de charactère affiché
+     * @param unite
+     */
+    public void setUnite(String unite){
+        this.unite = unite;
+    }
+
+    /**
+     *
+     * @return le BigInteger
+     */
+    public BigInteger getValue() {
+        return value;
+    }
+
+    public void setValue(BigInteger value){
+        this.value = value;
+    }
+
+    public void setValue(int value){
+        this.value = new BigInteger(value + "");
+    }
+
+    public void setValue(long value){
+        this.value = new BigInteger(value + "");
+    }
+
+    /**
+     * subtract cette valeur a la valeur passer en paramètre
+     * @param number
+     */
     public void subtract(BigValue number) {
     	value = value.subtract(number.getValue());
     }
@@ -51,13 +92,17 @@ public class BigValue {
     public void subtract(long number) {
     	subtract(new BigInteger(number+""));
     }
-    
+
+    /**
+     * Additionne cette valeur à la valeur passer en paramètre
+     * @param number
+     */
     public void add(BigValue number) {
-    	value = value.add(number.getValue());
+        if(value.add(number.getValue()).compareTo(maxValue) != 1)value = value.add(number.getValue());
     }
     
     public void add(BigInteger number) {
-    	value = value.add(number);
+        if(value.add(number).compareTo(maxValue) != 1)value = value.add(number);
     }
     
     public void add(int number) {
@@ -66,7 +111,10 @@ public class BigValue {
     
     public void add(long number) {
     	add(new BigInteger(number+""));
+    }
 
+    public void add(String number){
+        add(new BigInteger(number));
     }
     
     private String getSubDivition() {
@@ -74,7 +122,8 @@ public class BigValue {
     	while(numberOfChars % 3 != 0) numberOfChars++;
     	return divition[numberOfChars/3-1];
     }
-    
+
+
     /**
      * Affiche le bigvalue (ex : 789 $, 456 k$, 123 M$) si le caractere d'unité est '$'
      */
---
+++ core/src/fr/fireowls/fireband/util/Constant.java
@@ -0,0 +1,6 @@
+package fr.fireowls.fireband.util;
+
+
+public class Constant {
+    public static String divition[] = {"", "k", "M", "B", "T"};
+}
