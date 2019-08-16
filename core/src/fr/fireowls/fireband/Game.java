e69b1587a5f9fc0f0223035884523fe334941467
vaniscotte-geoffrey <vaniscotte.geoffrey@hotmail.com>
2019-08-16 22:13:34 +0200

    Create Game.java

---
+++ core/src/fr/fireowls/fireband/Game.java
@@ -0,0 +1,21 @@
+package fr.fireowls.fireband;
+
+import java.util.Scanner;
+
+import fr.fireowls.fireband.instruments.Piano;
+import fr.fireowls.fireband.player.Player;
+
+public class Game {
+    public static void main(String[] args){
+        Player player = new Player("GeoJo");
+        Scanner scan = new Scanner(System.in);
+        String str = "";
+
+        while(!str.equals("exit")) {
+            player.addInstrument(new Piano());
+            System.out.println(player.getName() + " --- " + player.getMoney() + " --- "/* + player.getCurrentInstrument()*/);
+            str = scan.nextLine();
+            //player.getCurrentInstrument().click();
+        }
+    }
+}
